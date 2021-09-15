01259a9fba611113649ab2c1eddecc6118922f18
phanthebao1234 <73651111+phanthebao1234@users.noreply.github.com>
2021-09-15 19:34:06 +0700

    Initial Generated Site

--- wp-login.php
+++ wp-login.php
@@ -90,7 +90,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/project\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://phanthebao1234.github.io/thebaowp/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"39552429da"};
+var userProfileL10n = {"user_id":"0","nonce":"a2858cc44d"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
